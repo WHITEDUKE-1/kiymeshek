b97c3f6647ea7c20b456d266ed44a33c92a38927
ASAD MAXSETOV <asadmaxsetov0@gmail.com>
2024-09-26 19:12:18 +0500

    Role modeli qosildi

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from src.db import db
+from src.dataloader import load_data
 
 def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+        load_data(db)
 
 # Bárshe modeller
 from .user_model import User
+from .role_model import Role
